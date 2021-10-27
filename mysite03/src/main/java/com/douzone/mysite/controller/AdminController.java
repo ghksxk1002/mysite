e1b04254a84b60e62887bd9e11647aa54aff1ce6
ghksxk1002 <ghksxk1002@naver.com>
2021-10-27 20:44:17 +0900

    ..

--- mysite03/src/main/java/com/douzone/mysite/controller/AdminController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/AdminController.java
@@ -3,12 +3,13 @@ package com.douzone.mysite.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.SystemPropertyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.douzone.mysite.security.Auth;
 import com.douzone.mysite.service.SiteService;
 import com.douzone.mysite.vo.SiteVo;
-
+@Auth(role="ADMIN")
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
@@ -16,27 +17,25 @@ public class AdminController {
 	@Autowired
 	SiteService siteService;
 
-	@Auth(role="ADMIN")
+	
 	@RequestMapping("")
 	public String main(Model model) {
 		SiteVo siteVo = siteService.getSite();
-		model.addAttribute(siteVo);
+		System.out.println(siteVo);
+		model.addAttribute("siteVo", siteVo);
 		return "admin/main";
 	}
 	
-	@Auth(role="ADMIN")
 	@RequestMapping("/guestbook")
 	public String guestbook() {
 		return "admin/guestbook";
 	}
 	
-	@Auth(role="ADMIN")
 	@RequestMapping("/board")
 	public String board() {
 		return "admin/board";
 	}
 	
-	@Auth(role="ADMIN")
 	@RequestMapping("/user")
 	public String user() {
 		return "admin/user";
