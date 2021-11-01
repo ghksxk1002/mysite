f6e70e6b79980d8530a7372f0139753023c0df6e
ghksxk1002 <ghksxk1002@naver.com>
2021-11-01 17:29:09 +0900

    mysite06 springboot 설정

--- mysite06/src/main/java/com/douzone/mysite/config/WebConfig.java
+++ mysite06/src/main/java/com/douzone/mysite/config/WebConfig.java
@@ -1,38 +1,103 @@
 package com.douzone.mysite.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import java.util.List;
 
-import com.douzone.config.web.FileUploadConfig;
-import com.douzone.config.web.MessageConfig;
-import com.douzone.config.web.MvcConfig;
-import com.douzone.config.web.SecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import com.douzone.mysite.interceptor.SiteInterceptor;
+import com.douzone.mysite.security.AuthInterceptor;
+import com.douzone.mysite.security.AuthUserHandlerMethodArgumentResolver;
+import com.douzone.mysite.security.LoginInterceptor;
+import com.douzone.mysite.security.LogoutInterceptor;
 
 @Configuration
-@EnableAspectJAutoProxy
-@ComponentScan({"com.douzone.mysite.controller", "com.douzone.mysite.exception"})
-@Import({MvcConfig.class, MessageConfig.class, FileUploadConfig.class, SecurityConfig.class})
-public class WebConfig extends WebMvcConfigurerAdapter{
+@PropertySource("classpath:com/douzone/mysite/config/WebConfig.properties")
+public class WebConfig implements WebMvcConfigurer {
 	
-	// siteIntercetors
+	@Autowired
+	private Environment env;
+	
+	// 아규먼트 리졸버
+	@Bean
+	public HandlerMethodArgumentResolver handlerMethodArgumentResolver() {
+		return new AuthUserHandlerMethodArgumentResolver();
+	}
+	
+	
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		argumentResolvers.add(handlerMethodArgumentResolver());
+	}
+	
+
+	// 인터셉터
 	@Bean
 	public HandlerInterceptor siteInterceptor() {
 		return new SiteInterceptor();
 	}
 	
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry
-			.addInterceptor(siteInterceptor())
-			.addPathPatterns("/**");
+	@Bean
+	public HandlerInterceptor loginInterceptor() {
+		return new LoginInterceptor();
+	}
+	
+	@Bean
+	public HandlerInterceptor logoutInterceptor() {
+		return new LogoutInterceptor();
+	}
+	
+	@Bean
+	public HandlerInterceptor authInterceptor() {
+		return new AuthInterceptor();
 	}
 	
 	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		registry
+		.addInterceptor(siteInterceptor())
+		.addPathPatterns("/**");
+		
+		registry
+			.addInterceptor(loginInterceptor())
+			.addPathPatterns("/user/auth");
+		
+		registry
+			.addInterceptor(logoutInterceptor())
+			.addPathPatterns("/user/logout");
+		
+		registry
+			.addInterceptor(authInterceptor())
+			.addPathPatterns("/**")
+			.excludePathPatterns("/user/auth")
+			.excludePathPatterns("/user/logout")
+			.excludePathPatterns("/assets/**");	
+		
+	}	
+	
+	// Default Servlet Hander
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	
+	// resourse Mapping(URL Magic Mapping) // 업로드 되는에 + 가상 url
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// 업로드 되는에 + 가상 url
+		registry
+			.addResourceHandler(env.getProperty("fileupload.resourceMapping"))
+			.addResourceLocations("file:"+env.getProperty("fileupload.uploadLocation"));
+	}
 }
